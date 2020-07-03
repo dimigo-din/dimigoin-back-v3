ac66584df5e3cdfc1814a5ecd7480cd86529698e
uhmseohun <uhmtotow@gmail.com>
2020-07-03 22:18:20 +0900

    feat(init): user model and model structure

---
+++ src/models/user.ts
@@ -0,0 +1,19 @@
+import { createSchema, ExtractDoc, Type, typedModel } from 'ts-mongoose'
+import { GenderValues, UserTypeValues, GradeValues, ClassValues } from '../types';
+
+export const userSchema = createSchema({
+  idx: Type.number({ required: true, unique: true }),
+  id: Type.string({ required: true, unique: true }),
+  name: Type.string({ required: true }),
+  gender: Type.string({ enum: GenderValues }),
+  phone: Type.string({ required: true }),
+  type: Type.string({ required: true, enum: UserTypeValues }),
+  grade: Type.number({ enum: GradeValues }),
+  class: Type.number({ enum: ClassValues }),
+  number: Type.number(),
+  serial: Type.number(),
+  photo: Type.array().of(Type.string()),
+});
+
+export type userDoc = ExtractDoc<typeof userSchema>;
+export const userModel = typedModel('User', userSchema);
