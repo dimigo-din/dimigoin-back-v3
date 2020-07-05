4283aa5d50a1118db04c08a97d593f078de4aa77
uhmseohun <uhmtotow@gmail.com>
2020-07-05 16:39:21 +0900

    refactor: assertion string type in config

--- src/config.ts
+++ src/config.ts
@@ -4,13 +4,13 @@ const env = dotenv.config();
 if (!env) throw new Error('No env file found');
 
 export default {
-  port: process.env.SERVER_PORT,
-  mongoId: process.env.MONGO_ID,
-  mongoPw: process.env.MONGO_PW,
-  mongoPort: process.env.MONGO_PORT,
-  mongoName: process.env.MONGO_NAME,
-  jwtSecret: process.env.JWT_SECRET,
-  apiId: process.env.DIMIAPI_ID,
-  apiPw: process.env.DIMIAPI_PW,
-  apiUrl: process.env.DIMIAPI_URL
+  port: process.env.SERVER_PORT!,
+  mongoId: process.env.MONGO_ID!,
+  mongoPw: process.env.MONGO_PW!,
+  mongoPort: process.env.MONGO_PORT!,
+  mongoName: process.env.MONGO_NAME!,
+  jwtSecret: process.env.JWT_SECRET!,
+  apiId: process.env.DIMIAPI_ID!,
+  apiPw: process.env.DIMIAPI_PW!,
+  apiUrl: process.env.DIMIAPI_URL!
 }
