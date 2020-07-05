f2ef15670ca45291d65b605331c2022e0e4d51bb
uhmseohun <uhmtotow@gmail.com>
2020-07-05 13:40:58 +0900

    feat(init): dimiapi resource

---
+++ src/resources/dimiapi.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import config from '../config';
+import { Account } from '../interfaces/dimiapi';
+
+/* eslint-disable */
+enum router {
+  getIdentity = '/v1/users/identify',
+  getAllUsers = '/v1/users',
+  getAllStudents = '/v1/user-students',
+};
+/* eslint-enable */
+
+const api = axios.create({
+  auth: {
+    username: config.apiId!,
+    password: config.apiPw!,
+  },
+  baseURL: config.apiUrl,
+});
+
+async function getIdentity(account: Account) {
+  const { data } = await api.get(
+    router.getIdentity,
+    { params: account },
+  );
+  return data;
+}
+
+export {
+  getIdentity,
+};
