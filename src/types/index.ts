21d6d16a087776c8c934aabd2e418d89c2b41b9d
uhmseohun <uhmtotow@gmail.com>
2020-07-05 13:40:39 +0900

    style: add missing semicolon

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,16 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express';
 
-export const GenderValues = ['M', 'F'] as const
+export const GenderValues = ['M', 'F'] as const;
 export type Gender = typeof GenderValues[number];
 
-export const UserTypeValues = ['S', 'O', 'D', 'T', 'P'] as const
+export const UserTypeValues = ['S', 'O', 'D', 'T', 'P'] as const;
 export type UserType = typeof UserTypeValues[number];
 
-export const GradeValues = [1, 2, 3] as const
+export const GradeValues = [1, 2, 3] as const;
 export type Grade = typeof GradeValues[number];
 
-export const ClassValues = [1, 2, 3, 4, 5, 6] as const
+export const ClassValues = [1, 2, 3, 4, 5, 6] as const;
 export type Class = typeof ClassValues[number];
 
 export type Middleware =
-  (req: Request, res: Response, next: NextFunction) => Promise<void>
+  (req: Request, res: Response, next: NextFunction) => Promise<void>;
