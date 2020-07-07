70e45dad7d2357a8ee821a619fdbd0a9769c4e73
cokia <hwymaster01@gmail.com>
2020-07-07 22:17:47 +0900

    add interface for dimi api

--- src/interfaces/dimiapi.ts
+++ src/interfaces/dimiapi.ts
@@ -1,4 +1,42 @@
-export interface Account {
+/* eslint-disable camelcase */
+
+import { ObjectId } from 'mongodb';
+import { Gender, UserType } from '../types';
+
+export interface IAccount {
   username: string;
   password: string;
+}
+
+export interface IUserIdentity {
+  _id: ObjectId;
+  id: number;
+  username: string;
+  email: string;
+  name: string;
+  nick: string;
+  gender: Gender;
+  user_type: UserType;
+  birthdate: string;
+  phone: string;
+  photofile1: string;
+  photofile2: string;
+}
+
+export interface IStudentIdentity {
+  user_id: number;
+  username: string;
+  email: string;
+  name: string;
+  grade: number;
+  class: number;
+  number: number;
+  serial: number;
+  nick: string;
+  gender: Gender;
+  user_type: UserType;
+  birthdate: string;
+  phone: string;
+  photofile1: string;
+  photofile2: string;
 }
