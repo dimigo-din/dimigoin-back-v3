d91de28323b5643cac757199abe0dbfa568b853e
uhmseohun <uhmtotow@gmail.com>
2020-08-21 12:07:57 +0900

    refactor: user-type check middleware

--- src/middlewares/check-user-type.ts
+++ src/middlewares/check-user-type.ts
@@ -5,12 +5,11 @@ import { UserType } from '../types';
 
 const TokenManager = new Token();
 
-const checkUserType = (userType: (UserType[] | 'U' | 'A')) => (req: Request, res: Response, next: NextFunction) => {
-  if (userType === 'U') return next(); // Route for UnAuthorized + Authorized
-  if (!req.token) throw new HttpException(403, '권한이 없습니다.');
+const checkUserType = (userType: (UserType[] | '*')) => (req: Request, res: Response, next: NextFunction) => {
+  if (!req.token) throw new HttpException(403, '토큰이 없습니다.');
   const { token } = req;
   const identity = TokenManager.verify(token);
-  if (userType === 'A') return next(); // Route for All Users
+  if (userType === '*') return next(); // Route for All Users
   if (userType.includes(identity.userType)) return next();
   throw new HttpException(403, '권한이 없습니다.');
 };
