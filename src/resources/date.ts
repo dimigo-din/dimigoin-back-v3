60ec3135610882adfc14923b7e9509ec69346e78
uhmseohun <uhmtotow@gmail.com>
2020-11-21 17:02:19 +0900

    feat: some implementation for date resource

--- src/resources/date.ts
+++ src/resources/date.ts
@@ -1,7 +1,17 @@
 export const getOnlyDate = (date: Date) => new Date(
-  date.getFullYear(),
-  date.getMonth(),
-  date.getDate(),
+  `${date.getFullYear()}-`
+  + `${date.getMonth() + 1}-`
+  + `${date.getDate()}`,
 );
 
 export const isValidDate = (date: Date) => !Number.isNaN(date.getTime());
+
+export const getWeekStart = (date: Date) => {
+  date.setDate(date.getDate() - date.getDay());
+  return date;
+};
+
+export const getWeekEnd = (date: Date) => {
+  date.setDate(date.getDate() - date.getDay());
+  return date;
+};
