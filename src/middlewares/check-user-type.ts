9ad4e27dcca79acd7158e8b822fe49ffc70b0ef3
uhmseohun <uhmtotow@gmail.com>
2020-11-21 10:07:08 +0900

    fix: token error handler

--- src/middlewares/check-user-type.ts
+++ src/middlewares/check-user-type.ts
@@ -5,12 +5,12 @@ import { UserType } from '../types';
 
 const checkUserType = (...userTypes: ((UserType | '*')[])) =>
   async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.token) throw new HttpException(403, '토큰이 없습니다.');
+    if (!req.token) return next(new HttpException(403, '토큰이 없습니다.'));
     const { token } = req;
     const identity = await verifyToken(token);
     if (userTypes[0] === '*') return next(); // Route for All Users
     if (userTypes.includes(identity.userType)) return next();
-    throw new HttpException(403, '권한이 없습니다.');
+    return next(new HttpException(403, '권한이 없습니다.'));
   };
 
 export default checkUserType;
