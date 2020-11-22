4da18d82db4cfbe4b6351bb3054129f4183e5e97
uhmseohun <uhmtotow@gmail.com>
2020-11-22 11:33:00 +0900

    refactor: exports all model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,17 +1,14 @@
 import 'ts-mongoose/plugin';
 
-export { AfterschoolModel } from './afterschool';
-export { UserModel } from './user';
-export { CircleModel } from './circle';
-export {
-  CircleApplicationModel,
-  CircleApplicationQuestionModel,
-} from './circle-application';
-export { ConfigModel } from './config';
-export { IngangApplicationModel } from './ingang-application';
-export { MealModel } from './meal';
-export { BookRequestModel } from './book-request';
-export { OutgoRequestModel } from './outgo-request';
+export * from './afterschool';
+export * from './user';
+export * from './circle';
+export * from './circle-application';
+export * from './config';
+export * from './ingang-application';
+export * from './meal';
+export * from './book-request';
+export * from './outgo-request';
 export * from './attendance-log';
-export { PlaceModel } from './place';
-export { NoticeModel } from './notice';
+export * from './place';
+export * from './notice';
