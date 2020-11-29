2c4c09f176ba5ed850449986f7a247d3fe4e9b83
cokia <hwymaster01@gmail.com>
2020-11-29 22:40:50 +0900

    type: add dets interface

--- src/interfaces/dets.ts
+++ src/interfaces/dets.ts
@@ -1,20 +1,22 @@
 import { ObjectId } from 'mongodb';
-import { TimeValues,DayValues,GradeValues } from '../types';
-  export interface IDets {
+import { TimeValues, DayValues, GradeValues } from '../types';
+
+export interface IDets {
     '_id': ObjectId;
     'title': string;
     'description': string;
     'startDate': Date;
     'endDate': Date;
+    'requestEndDate': Date;
     'speakerID': ObjectId;
     'speakerName': string;
     'time': typeof TimeValues;
     'day': typeof DayValues;
     'room': string;
     'maxCount': number;
+    'imageUrl': string;
     'targetGrade': typeof GradeValues;
     'count': number;
-    'status': boolean;
 }
 
 export interface IDetsInput {
@@ -22,10 +24,12 @@ export interface IDetsInput {
     'description': string;
     'startDate': Date;
     'endDate': Date;
+    'requestEndDate': Date;
     'time': typeof TimeValues;
     'speakerID': ObjectId;
-    'date': typeof DayValues;
+    'day': typeof DayValues;
     'room': string;
     'maxCount': number;
     'targetGrade': typeof GradeValues;
+    'imageUrl': string;
 }
