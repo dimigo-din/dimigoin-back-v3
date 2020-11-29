5b0479c4b06cbed5dc95631298550ae0f8abcd6e
cokia <hwymaster01@gmail.com>
2020-11-29 22:40:28 +0900

    add: /dets router

---
+++ src/routes/dets.ts
@@ -0,0 +1,54 @@
+import Joi from 'joi';
+import { Controller } from '../classes';
+import { validator, checkUserType } from '../middlewares';
+import {
+  getDetsList, getDets, createDets, editDets,
+} from '../controllers/dets';
+import wrapper from '../resources/wrapper';
+
+class NoticeController extends Controller {
+  public basePath = '/dets';
+
+  constructor() {
+    super();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get('/', checkUserType('*'), wrapper(getDetsList));
+
+    this.router.get('/:detsId', checkUserType('*'), wrapper(getDets));
+
+    this.router.post('/', checkUserType('T', 'S'), validator(Joi.object({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      startDate: Joi.date().required(),
+      endDate: Joi.date().required(),
+      requestEndDate: Joi.date().required(),
+      time: Joi.string().required(),
+      speakerID: Joi.string().required(),
+      day: Joi.string().required(),
+      room: Joi.string().required(),
+      maxCount: Joi.number().required(),
+      targetGrade: Joi.array().items(Joi.number()).required(),
+      imageUrl: Joi.string().required(),
+    })), wrapper(createDets));
+
+    this.router.patch('/:noticeId', checkUserType('T', 'S'), validator(Joi.object({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      startDate: Joi.date().required(),
+      endDate: Joi.date().required(),
+      requestEndDate: Joi.date().required(),
+      time: Joi.string().required(),
+      speakerID: Joi.string().required(),
+      day: Joi.string().required(),
+      room: Joi.string().required(),
+      maxCount: Joi.number().required(),
+      targetGrade: Joi.array().items(Joi.number()).required(),
+      imageUrl: Joi.string().required(),
+    })), wrapper(editDets));
+  }
+}
+
+export default NoticeController;
