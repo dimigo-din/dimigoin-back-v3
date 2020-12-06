84649ba1ca5c0e628141daedda2c71d71533570f
cokia <hwymaster01@gmail.com>
2020-12-07 08:48:39 +0900

    fix: add enum type to time/day/grade

--- src/models/dets.ts
+++ src/models/dets.ts
@@ -3,6 +3,7 @@ import {
   createSchema, ExtractDoc, Type, typedModel,
 } from 'ts-mongoose';
 import { userSchema } from './user';
+import { TimeValues, DayValues, GradeValues } from '../types';
 
 const detsSchema = createSchema({
   title: Type.string({ required: true, unique: true, trim: true }),
@@ -10,12 +11,12 @@ const detsSchema = createSchema({
   startDate: Type.date({ required: true }),
   endDate: Type.date({ required: true }),
   requestEndDate: Type.date({ required: true }),
-  time: Type.array({ required: true }), // add time type validation
+  time: Type.array({ required: true, enum: TimeValues }), // add time type validation
   speakerID: Type.ref(Type.objectId()).to('User', userSchema),
-  day: Type.array({ required: true }), // add day type validation
+  day: Type.array({ required: true, enum: DayValues }), // add day type validation
   room: Type.string({ required: true }), // add room type validation
   maxcount: Type.number({ required: true }),
-  targetGrade: Type.number({ required: true }), // add grade validation
+  targetGrade: Type.number({ required: true, enum: GradeValues }), // add grade validation
   imageUrl: Type.string({ required: true }),
   count: Type.number({ required: true }),
   user: [Type.string({ required: true })],
