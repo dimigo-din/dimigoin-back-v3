99d15572a09b8be8b07603e91d62f4931e1fd8b6
Seohun Uhm <uhmtotow@gmail.com>
2021-01-09 14:45:44 +0900

    fix: token expired handler

--- src/middlewares/attach-user-info.ts
+++ src/middlewares/attach-user-info.ts
@@ -1,12 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
+import { HttpException } from '../exceptions';
 import { verify as verifyToken } from '../resources/token';
 
-const attachUserInfo = (req: Request, res: Response, next: NextFunction) => {
+const attachUserInfo = async (req: Request, res: Response, next: NextFunction) => {
   if (!req.token) { return next(); }
   const { token } = req;
-  const identity = verifyToken(token);
-  req.app.set('user', identity);
-  next();
+  try {
+    const identity = await verifyToken(token);
+    req.app.set('user', identity);
+    next();
+  } catch (error) {
+    return next(
+      new HttpException(403, '토큰이 정상적으로 검증되지 않았습니다.'),
+    );
+  }
 };
 
 export default attachUserInfo;
