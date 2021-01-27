3a8eff51aa65e96c4209f397fef7cf0e71d06267
Seohun Uhm <uhmtotow@gmail.com>
2021-01-27 21:38:39 +0900

    fix: remove wrong importing statement

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,7 @@
 import RootController from './root';
-import CircleApplicationManagementController from './circle-application-management';
 
 const controllers = [
   new RootController(),
-  new CircleApplicationManagementController(),
 ];
 
 export default controllers;
