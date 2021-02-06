835ea995c37a31189cd67507b10ea3ae7509a329
rycont <reactdev@kakao.com>
2021-02-06 23:46:57 +0900

    fix : apply changes to create controller

--- src/services/ingang-application/controllers.ts
+++ src/services/ingang-application/controllers.ts
@@ -74,7 +74,7 @@ export const createIngangApplication = async (req: Request, res: Response) => {
   }
 
   const todayAll = await IngangApplicationModel.find({
-    today,
+    date: today,
     time,
   }).populateTs('applier');
 
@@ -95,7 +95,7 @@ export const createIngangApplication = async (req: Request, res: Response) => {
     ...req.body,
     time,
     applier,
-    today,
+    date: today,
   }).save();
   res.json({ ingangApplication });
 };
