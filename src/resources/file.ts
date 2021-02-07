72e937587e72410a6987019c8a1c9a590eca08ca
uhmseohun <uhmtotow@gmail.com>
2021-02-07 23:51:14 +0900

    feat: implement downloadLimit (with default value)

--- src/resources/file.ts
+++ src/resources/file.ts
@@ -1,12 +1,14 @@
 import fs from 'fs';
-import config from '../config';
 import { join as pathJoin } from 'path';
+import config from '../config';
 import { FileModel } from '../models';
 import { IUser } from '../interfaces';
 
-export const writeFile = async (buffer: Buffer, name: string, extension: string, user: IUser) => {
-    const file = await new FileModel({ name, extension, owner: user._id }).save();
-    const path = pathJoin(config.fileStoragePath, file._id.toString());
-    fs.writeFileSync(path, buffer);
-    return file;
+export const writeFile = async (buffer: Buffer, name: string, extension: string, user: IUser, downloadLimit: number = 0) => {
+  const file = await new FileModel({
+    name, extension, downloadLimit, owner: user._id,
+  }).save();
+  const path = pathJoin(config.fileStoragePath, file._id.toString());
+  fs.writeFileSync(path, buffer);
+  return file;
 };
