2f50a5fdd8393751e3c70e26e04a757db4225a97
uhmseohun <uhmtotow@gmail.com>
2021-02-07 23:44:46 +0900

    feat: implement file download route

---
+++ src/services/file/index.ts
@@ -0,0 +1,13 @@
+import * as controllers from './controllers';
+
+export default {
+  name: '파일 다운로드 서비스',
+  baseURL: '/file',
+  routes: [
+    {
+      method: 'get',
+      path: '/:fileId',
+      handler: controllers.downloadFile,
+    },
+  ],
+};
