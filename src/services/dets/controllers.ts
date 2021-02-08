1a1eca356f1696e4512c09f2923c1cc328fa10de
cokia <hwymaster01@gmail.com>
2021-02-08 11:12:43 +0900

    fix: add get/create/edit dets conroller

---
+++ src/services/dets/controllers.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { HttpException } from '../../exceptions';
+import { DetsModel } from '../../models';
+
+export const getDetsList = async (req: Request, res: Response) => {
+  const { userType, grade } = req.user;
+  const Dets = await DetsModel.find(
+    userType === 'T' ? {} : { targetGrade: { $all: [grade] } },
+  );
+  res.json({ Dets });
+};
+
+export const getDets = async (req: Request, res: Response) => {
+  const dets = await DetsModel.findById(req.params.detsId);
+  res.json({ dets });
+};
+
+export const createDets = async (req: Request, res: Response) => {
+  const detsData = req.body;
+  detsData.speakerID = req.user._id;
+  const dets = new DetsModel(detsData);
+  await dets.save();
+  res.json({ dets });
+};
+
+export const editDets = async (req: Request, res: Response) => {
+  const dets = await DetsModel.findById(req.params.detsId);
+  if (!dets) throw new HttpException(404, '해당 뎃츠를 찾을 찾을 수 없습니다.');
+  if (dets.speakerID === req.user._id) {
+    Object.assign(dets, req.body);
+    await dets.save();
+    res.json({ dets });
+  }
+  if (dets.speakerID !== res.locals.user.id) {
+    throw new HttpException(403, '해당 뎃츠의 소유자가 아닙니다.');
+  }
+};
