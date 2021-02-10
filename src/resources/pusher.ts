d15be6e17ac1bd04abf3a831f363051504cc391d
uhmseohun <uhmtotow@gmail.com>
2021-02-10 22:36:11 +0900

    feat: implement pusher resource

---
+++ src/resources/pusher.ts
@@ -0,0 +1,20 @@
+import admin from 'firebase-admin';
+import config from '../config';
+import { UserModel } from '../models';
+
+admin.initializeApp({
+  credential: admin.credential.cert(
+    'firebase-service-account.json',
+  ),
+  databaseURL: config.firebaseDatabaseURL,
+});
+
+export const sendPushMessage = async (userFilter: object, title: string, body: string) => {
+  const users = await UserModel.find(userFilter).select('tokens');
+  const userTokens = users.reduce((t, u) => [...t, ...(u.tokens)], []);
+  const message = { title, body };
+  await admin.messaging().sendMulticast({
+    notification: message,
+    tokens: userTokens,
+  });
+};
