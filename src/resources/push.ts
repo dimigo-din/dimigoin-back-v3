db7b361369dd4d3fbdae157245acaf2746c81da9
uhmseohun <uhmtotow@gmail.com>
2021-02-10 23:11:12 +0900

    feat: split tokens and send push message

--- src/resources/push.ts
+++ src/resources/push.ts
@@ -11,10 +11,22 @@ admin.initializeApp({
 
 export const sendPushMessage = async (userFilter: object, title: string, body: string) => {
   const users = await UserModel.find(userFilter).select('tokens');
-  const userTokens = users.reduce((t, u) => [...t, ...(u.tokens)], []);
   const message = { title, body };
-  return await admin.messaging().sendMulticast({
-    notification: message,
-    tokens: userTokens,
-  });
+  const userTokens = users.reduce((t, u) => [...t, ...(u.tokens)], []);
+
+  // Firebase에서 한 번 호출 시에 최대 500개의 토큰까지만 지원함
+  const devidedUserTokens = Array.from(
+    { length: Math.ceil(userTokens.length / 500) },
+    () => userTokens.splice(0, 500),
+  );
+
+  const results = [];
+  for (const tokens of devidedUserTokens) {
+    const result = await admin.messaging().sendMulticast({
+      notification: message,
+      tokens,
+    });
+    results.push(result);
+  }
+  return results;
 };
