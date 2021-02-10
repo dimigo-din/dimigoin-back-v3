a97dad872b4dcf57abe768e38ad52f2ed8eb28a5
엄서훈 <uhmtotow@gmail.com>
2021-02-11 00:44:30 +0900

    feat: add some cron jobs (#33)
    * feat: remove /user/reloadd endpoint
    
    * refactor: split setCronJobsAndRun into two functions
    
    * feat: implement running cron jobs endpoint
    
    * feat: remove /user/reloadd endpoint
    
    * refactor: split setCronJobsAndRun into two functions
    
    * feat: implement running cron jobs endpoint
    
    * feat: implement ingang appliers notification

--- src/resources/cron.ts
+++ src/resources/cron.ts
@@ -1,21 +1,54 @@
 import cron from 'node-cron';
 import { refreshWeeklyTimetable } from './timetable';
 import {
+  notifyIngangAppliers,
+} from './notifier';
+import {
   reloadAllUsers,
   attachStudentInfo,
 } from './dimi-api';
 
-export const setCronJobsAndRun = async () => {
-  const cronJobs = [
-    { schedule: '0 */2 * * *', action: refreshWeeklyTimetable },
-    { schedule: '0 0 * * *', action: async () => {
+const cronJobs = [
+  {
+    schedule: '0 */2 * * *',
+    action: refreshWeeklyTimetable,
+    runOnSetup: true,
+  },
+  {
+    schedule: '0 0 * * *',
+    action: async () => {
       await reloadAllUsers();
       await attachStudentInfo();
-    } },
-  ];
-  
+    },
+    runOnSetup: true,
+  },
+  // 인강실 신청자 알림 (1타임)
+  {
+    schedule: '35 19 * * 1-5',
+    action: async () => await notifyIngangAppliers('NSS1'),
+    runOnSetup: false,
+  },
+  // 인강실 신청자 알림 (2타임)
+  {
+    schedule: '15 21 * * 1-5',
+    action: async () => await notifyIngangAppliers('NSS2'),
+    runOnSetup: false,
+  },
+];
+
+export const setCronJobs = async () => {
   for (const { schedule, action } of cronJobs) {
-    await action();
     cron.schedule(schedule, action);
   }
+};
+
+export const manuallyRunCronJobs = async () => {
+  for (const { action } of cronJobs) {
+    await action();
+  }
+};
+
+export const setCronJobsAndRun = async () => {
+  await setCronJobs();
+  await manuallyRunCronJobs();
 };
