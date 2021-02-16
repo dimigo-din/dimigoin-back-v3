062338d4ee6a8cf6f29d7ca97b7771f06ac07fb7
Seohun Uhm <uhmtotow@gmail.com>
2021-02-17 00:25:25 +0900

    feat: send push notification immediately for today notice

--- src/services/notice/controllers.ts
+++ src/services/notice/controllers.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { sendPushMessage } from '../../resources/push';
 import { HttpException } from '../../exceptions';
 import { NoticeModel } from '../../models';
 import { getTodayDateString } from '../../resources/date';
@@ -48,5 +49,15 @@ export const editNotice = async (req: Request, res: Response) => {
 
 export const createNotice = async (req: Request, res: Response) => {
   const notice = await new NoticeModel(req.body).save();
+
+  const today = getTodayDateString();
+  if (today === notice.startDate) {
+    await sendPushMessage(
+      { grade: { $in: notice.targetGrade } },
+      '새로운 공지사항이 등록되었어요!',
+      `[${notice.title}]\n${notice.content}`,
+    );
+  }
+
   res.json({ notice });
 };
