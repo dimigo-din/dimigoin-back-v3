66c5d1e8c220a349136a472b64d03dfc2e5d7c47
uhmseohun <uhmtotow@gmail.com>
2021-02-19 23:23:59 +0900

    chore: remove legacy middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,3 @@
 export { default as attachUserInfo } from './attach-user-info';
-export { default as checkUserType } from './check-user-type';
 export { default as errorHandler } from './error-handler';
 export { default as validator } from './validator';
