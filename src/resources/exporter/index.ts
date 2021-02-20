babc225cab86e4a0a65eff08ba01e31920dec872
uhmseohun <uhmtotow@gmail.com>
2021-02-20 23:58:58 +0900

    chore: remove useless exporting util

--- src/resources/exporter/index.ts
+++ src/resources/exporter/index.ts
@@ -1,7 +1,5 @@
 import exceljs from 'exceljs';
 
-export const createStandardBuffer = (eBuffer: exceljs.Buffer) => Buffer.from(eBuffer.toString());
-
 export const setAlignCenter = (sheet: exceljs.Worksheet, cellKey: string) => {
   sheet.getCell(cellKey).alignment = {
     horizontal: 'center',
