a423078ea49ee531fc7e706471a2341b7a8857e4
uhmseohun <uhmtotow@gmail.com>
2021-02-20 23:57:27 +0900

    fix: allow Exceljs buffer in writeFile resource

--- src/resources/file.ts
+++ src/resources/file.ts
@@ -1,14 +1,16 @@
 import fs from 'fs';
+import exceljs from 'exceljs';
 import { join as pathJoin } from 'path';
 import config from '../config';
 import { FileModel } from '../models';
 import { IUser } from '../interfaces';
 
-export const writeFile = async (buffer: Buffer, name: string, extension: string, user: IUser, downloadLimit: number = 0) => {
+export const writeFile = async (buffer: Buffer | exceljs.Buffer, name: string, extension: string, user: IUser, downloadLimit: number = 0) => {
   const file = await new FileModel({
     name, extension, downloadLimit, owner: user._id,
   }).save();
   const path = pathJoin(config.fileStoragePath, file._id.toString());
+  // @ts-ignore (Buffer Type 관련)
   fs.writeFileSync(path, buffer);
   return file;
 };
