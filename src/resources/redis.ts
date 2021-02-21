369694b2f2a61e42e1f67dc584c62971986560de
uhmseohun <uhmtotow@gmail.com>
2021-02-21 12:29:11 +0900

    feat: implement redis functions

--- src/resources/redis.ts
+++ src/resources/redis.ts
@@ -14,4 +14,22 @@ client.on('error', (error) => {
   logger.error(error);
 });
 
-export default client;
+export const set = async (key: string, value: any) => {
+  await client.set(key, value);
+};
+
+export const get = async (key: string) => await client.get(key);
+
+export const setHash = async (name: string, key: string, value: any) => {
+  await client.hmset(name, key, value);
+};
+
+export const getHash = async (name: string, key: string) => await client.hmget(name, key);
+
+export const getAllHashes = async (name: string) => await client.hgetall(name);
+
+export const checkExist = async (key: string) => await client.exists(key);
+
+export const deleteKey = async (key: string) => {
+  await client.del(key);
+};
