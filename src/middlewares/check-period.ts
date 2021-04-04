3a2f6a96b0211e451b01fb28b9fdaa49edfc228f
cokia <hwymaster01@gmail.com>
2021-04-04 16:58:58 +0900

    fix: status code for invaild applyPeriod return

--- src/middlewares/check-period.ts
+++ src/middlewares/check-period.ts
@@ -11,7 +11,7 @@ export const checkAfterschoolApplyPeriod = async (req: Request, res: Response, n
 
   const now = new Date();
   if (now < applyPeriod.start || applyPeriod.end < now) {
-    throw new HttpException(403, '방과 후 수강 신청 기간이 아닙니다.');
+    throw new HttpException(400, '방과 후 수강 신청 기간이 아닙니다.');
   }
   next();
 };
@@ -25,7 +25,7 @@ export const checkIngangApplyPeriod = async (req: Request, res: Response, next: 
     minute: moment().minute(),
   });
   if (now < applyStart || applyEnd < now) {
-    throw new HttpException(403, '인강실 추가 신청 및 취소가 가능한 시간이 아닙니다.');
+    throw new HttpException(400, '인강실 추가 신청 및 취소가 가능한 시간이 아닙니다.');
   }
   next();
 };
@@ -39,7 +39,7 @@ export const checkMetoringApplyPeriod = async (req: Request, res: Response, next
     minute: moment().minute(),
   });
   if (now < applyStart || applyEnd < now) {
-    throw new HttpException(403, '멘토링 신청 및 취소가 가능한 시간이 아닙니다.');
+    throw new HttpException(400, '멘토링 신청 및 취소가 가능한 시간이 아닙니다.');
   }
   next();
 };
