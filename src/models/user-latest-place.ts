cb98a55ae763c6430b079e83b85f0061c2d6ee8a
cokia <hwymaster01@gmail.com>
2021-04-19 10:51:30 +0900

    fix: update user-latest-place schema

--- src/models/user-latest-place.ts
+++ src/models/user-latest-place.ts
@@ -1,13 +1,11 @@
 import {
   createSchema, Type, typedModel, ExtractDoc,
 } from 'ts-mongoose';
-import { PlaceType } from '../types';
 
 const userLatestPlaceSchema = createSchema({
   userId: Type.objectId({ required: true, unique: true }),
-  location: Type.string({ required: true, trim: true }),
+  location: Type.objectId({ required: true }),
   description: Type.string({ trim: true }),
-  type: Type.string({ required: true, enum: Object.values(PlaceType), default: 'ETC' }),
 }, { versionKey: false, timestamps: true });
 
   type userLatestPlaceDocs = ExtractDoc<typeof userLatestPlaceSchema>;
