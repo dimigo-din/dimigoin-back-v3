7d9024f4ad600801e27b7aa9190a00da15fe7844
오명훈 <omh02033@gmail.com>
2022-09-09 12:27:37 +0900

    fix: meal push notification

--- src/resources/notifier.ts
+++ src/resources/notifier.ts
@@ -50,6 +50,7 @@ export const notifyMealMenu = async (targetGrades: Grade[], time: MealTime) => {
   const meal = await MealModel.findOne({ date: today });
   if (!meal) return;
   if (!(time as string in meal)) return;
+  if (meal[time].toString() === '급식 정보가 없습니다.') return;
   await sendPushMessage(
     { grade: { $in: targetGrades } },
     `오늘의 ${mealString[time]} 메뉴를 확인하세요!`,
