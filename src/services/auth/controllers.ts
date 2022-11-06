e266d8604d45f6f8c62285b291c4e3ac7e77fef8
오명훈 <omh02033@gmail.com>
2022-11-06 22:19:10 +0900

    fix: login logic

--- src/services/auth/controllers.ts
+++ src/services/auth/controllers.ts
@@ -3,40 +3,22 @@ import { HttpException } from '../../exceptions';
 import { Account } from '../../interfaces/dimi-api';
 import { CircleModel, PermissionModel } from '../../models';
 import { issue as issueToken, verify, getTokenType } from '../../resources/token';
-import { getIdentity, getStudentInfo, getTeacherInfo } from '../../resources/dimi-api';
+import { getIdentity, getStudentInfo } from '../../resources/dimi-api';
 
 const getExtraPermissions = async (userIdx: number) => {
-  const user = await getStudentInfo(userIdx);
-  const permissions = [];
-
-  // (부)동아리장은 동아리원 선발 서비스 권한 부여
-  const circle = await CircleModel.findByChairs(user.user_id);
-  if (circle) {
-    permissions.push('circle-applier-selection');
-  }
-
-  return permissions;
-};
-
-const getEntireIdentity = async (userIdx: number) => {
-  const { permissions } = await PermissionModel.findOne({ userId: userIdx });
   try {
-    const identity = await getStudentInfo(userIdx);
-    if (!identity) throw new HttpException(404, '해당 사용자를 찾을 수 없습니다.');
-    identity.permissions = permissions;
-    identity.permissions.push(
-      ...await getExtraPermissions(userIdx),
-    );
-    return identity;
-  } catch (e) {
-    try {
-      const identity = await getTeacherInfo(userIdx);
-      if (!identity) throw new HttpException(404, '해당 사용자를 찾을 수 없습니다.');
-      identity.permissions = permissions;
-      return identity;
-    } catch (err) {
-      throw new HttpException(404, '해당 사용자를 찾을 수 없습니다.');
+    const user = await getStudentInfo(userIdx);
+    const permissions = [];
+
+    // (부)동아리장은 동아리원 선발 서비스 권한 부여
+    const circle = await CircleModel.findByChairs(user.user_id);
+    if (circle) {
+      permissions.push('circle-applier-selection');
     }
+
+    return permissions;
+  } catch (e) {
+    return [];
   }
 };
 
@@ -45,13 +27,18 @@ export const identifyUser = async (req: Request, res: Response) => {
   const { dalgeurak } = req.query;
 
   try {
-    const { id: idx, dalgeurakFirstLogin } = await getIdentity(account, dalgeurak as string);
-    const identity = await getEntireIdentity(idx);
+    const identity = await getIdentity(account, dalgeurak as string);
+
+    const { permissions } = await PermissionModel.findOne({ userId: identity.id });
+    identity.permissions = permissions;
+    identity.permissions.push(
+      ...await getExtraPermissions(identity.id),
+    );
 
     res.json({
       accessToken: await issueToken(identity, false),
       refreshToken: await issueToken(identity, true),
-      dalgeurakFirstLogin,
+      dalgeurakFirstLogin: identity.dalgeurakFirstLogin,
     });
   } catch (error) {
     if (error.name === 'HttpException') throw error;
@@ -67,9 +54,8 @@ export const refreshAccessToken = async (req: Request, res: Response) => {
   if (tokenType !== 'REFRESH') throw new HttpException(400, '리프레시 토큰이 아닙니다.');
 
   const payload = await verify(refreshToken);
-  const identity = await getEntireIdentity(payload.user_id);
   res.json({
-    accessToken: await issueToken(identity, false),
-    refreshToken: await issueToken(identity, true),
+    accessToken: await issueToken(payload, false),
+    refreshToken: await issueToken(payload, true),
   });
 };
