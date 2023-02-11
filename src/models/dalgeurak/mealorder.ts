7877a3e57ba00815d7d36201c16435cda5a39fcb
오명훈 <omh02033@gmail.com>
2023-02-11 22:44:53 +0900

    fix: add meal order breakfast data

--- src/models/dalgeurak/mealorder.ts
+++ src/models/dalgeurak/mealorder.ts
@@ -4,12 +4,14 @@ import { dalgeurakDB } from '../../resources/dalgeurakDB';
 import { MealOrderValues, ClassType } from '../../types';
 
 export interface ImealOrder extends Document {
+  breakfast?: Array<ClassType>;
   lunch?: Array<ClassType>;
   dinner?: Array<ClassType>;
   field: 'sequences' | 'times';
 }
 
 const mealOrderSchema = createSchema({
+  breakfast: Type.array().of([Number]),
   lunch: Type.array().of([Number]),
   dinner: Type.array().of([Number]),
   field: Type.string({ enum: MealOrderValues, required: true }),
